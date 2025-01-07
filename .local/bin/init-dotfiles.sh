986f844e964d6ede2b01e937b97d65dd54e43f36
Dave Burke <dburke84@gmail.com>
2025-01-06 18:49:21 -0600

    init-dotfiles: Fix a few bugs

--- .local/bin/init-dotfiles.sh
+++ .local/bin/init-dotfiles.sh
@@ -4,6 +4,8 @@ set -e
 
 /usr/bin/git clone --bare git@github.com:dave-burke/dotfiles $HOME/.dotfiles
 
+cd $HOME
+
 dotfiles() {
     /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME "$@"
 }
@@ -14,13 +16,13 @@ dotfiles config --local status.showUntrackedFiles no
 echo "Setting up sparse checkout..."
 dotfiles config --local core.sparseCheckout true
 echo '/*' > .dotfiles/info/sparse-checkout
-echo '!README.md' >> .dotfiles/info/sparse-checkout
+echo '!/README.md' >> .dotfiles/info/sparse-checkout
 
 echo "Backing up conflicting files..."
 mkdir -p $HOME/.dotfiles-backup
 dotfiles checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | while read -r file; do
   mv -v "$file" "$HOME/.dotfiles-backup/"
 done
-dotfiles checkout
+dotfiles checkout -f
 
 echo "Done. Don't forget to source .bashrc or .zshrc"
