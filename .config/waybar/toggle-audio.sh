d200cc4abfd0c7d7c46fb23c41091e8f60969fbf
Dave Burke <dburke84@gmail.com>
2025-02-25 07:37:40 -0600

    waybar: toggle source with sink

--- .config/waybar/toggle-audio.sh
+++ .config/waybar/toggle-audio.sh
@@ -5,25 +5,33 @@ current_sink=$(pactl get-default-sink)
 
 # List all available sinks
 available_sinks=$(pactl list short sinks | awk '{print $2}')
+available_sources=$(pactl list short sources | awk '{print $2}')
 
 # Check if current sink is already set to headphones or speakers and toggle accordingly
 if [[ "$current_sink" != *Razer_Barracuda_X* ]]; then
   for sink in $available_sinks; do
     if [[ "$sink" == *Razer_Barracuda_X* ]]; then
       pactl set-default-sink "$sink"
-      echo "Switched to: $sink (Headphones)"
-      exit 0
+      echo "Switched sink to: $sink (Headphones)"
+    fi
+  done
+  for source in $available_sources; do
+    if [[ "$source" == *input*Razer_Barracuda_X* ]]; then
+      pactl set-default-source "$source"
+      echo "Switched source to: $source (Headphones)"
     fi
   done
 else
   for sink in $available_sinks; do
     if [[ "$sink" == *pci-0000* ]]; then
       pactl set-default-sink "$sink"
-      echo "Switched to: $sink (Speakers)"
-      exit 0
+      echo "Switched sink to: $sink (Speakers)"
+    fi
+  done
+  for source in $available_sources; do
+    if [[ "$source" == *input*pci-0000* ]]; then
+      pactl set-default-source "$source"
+      echo "Switched source to: $source (Built-in)"
     fi
   done
 fi
-
-echo "No switch performed. Could not find matching sinks."
-exit 1
